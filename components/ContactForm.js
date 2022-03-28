6adf5c5e0b833adab19f8a1d5ab313c4819b13e0
Roy Sheppard <rsheppard83@gmail.com>
2022-03-28 11:35:55 +0100

    styled dialogue

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,26 +1,81 @@
-import { useState } from 'react';
-import { Dialog } from '@headlessui/react';
+import { useState, Fragment } from 'react';
+import { Dialog, Transition } from '@headlessui/react';
 
 const ContactForm = () => {
 	const [isOpen, setIsOpen] = useState(false);
 
+	const openModal = () => setIsOpen(true);
+	const closeModal = () => setIsOpen(false);
+
 	const handleSubmit = e => {
 		e.preventDefault();
-		setIsOpen(true);
+		openModal();
 	};
 	return (
 		<>
-			<Dialog open={isOpen} onClose={() => setIsOpen(false)}>
-				<Dialog.Overlay />
+			<Transition appear show={isOpen} as={Fragment}>
+				<Dialog
+					as='div'
+					className='fixed inset-0 z-10 overflow-y-auto'
+					onClose={closeModal}
+				>
+					<div className='min-h-screen px-4 text-center'>
+						<Transition.Child
+							as={Fragment}
+							enter='ease-out duration-300'
+							enterFrom='opacity-0'
+							enterTo='opacity-100'
+							leave='ease-in duration-200'
+							leaveFrom='opacity-100'
+							leaveTo='opacity-0'
+						>
+							<Dialog.Overlay className='fixed inset-0 bg-black opacity-30' />
+						</Transition.Child>
 
-				<Dialog.Title>Message Sent</Dialog.Title>
-				<Dialog.Description>
-					Thank you for getting in touch. I will get back to you as soon as
-					possible.
-				</Dialog.Description>
+						{/* This element is to trick the browser into centering the modal contents. */}
+						<span
+							className='inline-block h-screen align-middle'
+							aria-hidden='true'
+						>
+							&#8203;
+						</span>
+						<Transition.Child
+							as={Fragment}
+							enter='ease-out duration-300'
+							enterFrom='opacity-0 scale-95'
+							enterTo='opacity-100 scale-100'
+							leave='ease-in duration-200'
+							leaveFrom='opacity-100 scale-100'
+							leaveTo='opacity-0 scale-95'
+						>
+							<div className='inline-block max-w-md p-6 my-8 text-left align-middle transition-all transform bg-accent-100 shadow-xl rounded-2xl'>
+								<Dialog.Title
+									as='h3'
+									className='font-medium leading-6 text-primary-100'
+								>
+									Message Sent
+								</Dialog.Title>
+								<div className='mt-5'>
+									<p className='text-white'>
+										Thank you for getting in touch. I will get back to you
+										within the next 24 hours.
+									</p>
+								</div>
 
-				<button onClick={() => setIsOpen(false)}>Close</button>
-			</Dialog>
+								<div className='mt-10'>
+									<button
+										type='button'
+										className='inline-flex justify-center px-4 py-2 font-medium text-white bg-secondary-100 border border-transparent rounded-md hover:bg-secondary-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-white'
+										onClick={closeModal}
+									>
+										Got it, thanks!
+									</button>
+								</div>
+							</div>
+						</Transition.Child>
+					</div>
+				</Dialog>
+			</Transition>
 
 			<form
 				name='contact'
@@ -42,7 +97,7 @@ const ContactForm = () => {
 						name='name'
 						type='text'
 						required='true'
-						className='rounded-lg focus:outline-none focus:ring focus:ring-gray-100 p-2'
+						className='text-gray-700 rounded-lg focus:outline-none focus:ring focus:ring-gray-100 p-2'
 					/>
 				</div>
 				<div className='flex flex-col space-y-2'>
@@ -52,7 +107,7 @@ const ContactForm = () => {
 						id='phone'
 						name='phone'
 						required='true'
-						className='rounded-lg focus:outline-none focus:ring focus:ring-gray-100 p-2'
+						className='text-gray-700 rounded-lg focus:outline-none focus:ring focus:ring-gray-100 p-2'
 					/>
 				</div>
 				<div className='flex flex-col space-y-2'>
@@ -62,7 +117,7 @@ const ContactForm = () => {
 						name='email'
 						type='text'
 						required='true'
-						className='rounded-lg focus:outline-none focus:ring focus:ring-gray-100 p-2'
+						className='text-gray-700 rounded-lg focus:outline-none focus:ring focus:ring-gray-100 p-2'
 					/>
 				</div>
 				<div className='flex flex-col space-y-2'>
@@ -72,7 +127,7 @@ const ContactForm = () => {
 						name='message'
 						rows='10'
 						required='true'
-						className='rounded-lg focus:outline-none focus:ring focus:ring-gray-100 p-2'
+						className='text-gray-700 rounded-lg focus:outline-none focus:ring focus:ring-gray-100 p-2'
 					/>
 				</div>
 				<button
