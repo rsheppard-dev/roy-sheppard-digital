56f05f7b20f0e9f4642c5e38b8e2b437d572be6e
Roy Sheppard <rsheppard83@gmail.com>
2022-05-01 07:24:10 +0100

    updated header

--- pages/web-development-watford.js
+++ pages/web-development-watford.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Image from 'next/image';
 
 import Layout from '../components/Layout';
 import Meta from '../components/Meta';
@@ -11,12 +12,26 @@ const WebDevelopment = () => {
 				<Meta />
 				<title>Web Development - Watford || Roy Sheppard Digital</title>
 			</Head>
-			<section className='container mt-40 text-accent-100'>
-				<div className='h-2 w-1/2 bg-primary-100 mx-auto my-10'></div>
-				<h1 className='text-center text-3xl md:text-6xl font-bold mb-10'>
-					Web Development - Watford
-				</h1>
-				<div className='shadow-md my-20 text-center rounded-lg bg-secondary-100/30 px-10 py-8 text-gray-600 font-futura'>
+			<header>
+				<div className='relative aspect-h-3 sm:aspect-h-2 aspect-w-4 z-10 inset-0 bg-cover bg-center bg-[url("https://res.cloudinary.com/roy-sheppard-digital/image/upload/e_pixelate:5,f_auto,q_0/v1643532339/roy-sheppard-digital/freelance-web-designer-watford.jpg")]'>
+					<Image
+						src='https://res.cloudinary.com/roy-sheppard-digital/image/upload/f_auto,q_auto/v1643532339/roy-sheppard-digital/freelance-web-designer-watford.jpg'
+						alt='Freelance web developer in Watford'
+						layout='fill'
+						objectFit='cover'
+						objectPosition='center'
+						priority={true}
+					/>
+					<div className='absolute z-20  inset-0 h-full w-full bg-black/30 mix-blend-hue'></div>
+					<div className='container absolute inset-0 justify-center items-center flex flex-col text-center z-20'>
+						<h1 className='mt-20 block font-futura font-bold text-white text-shadow text-3xl md:text-6xl'>
+							Web Development - Watford
+						</h1>
+					</div>
+				</div>
+			</header>
+			<section className='container text-accent-100 relative'>
+				<div className='my-10 shadow-md shadow-secondary-200 text-center rounded-lg bg-secondary-100 px-10 py-8 text-white font-futura'>
 					<blockquote className='text-2xl'>
 						42% of people will leave a website because of poor functionality.
 					</blockquote>
@@ -39,10 +54,10 @@ const WebDevelopment = () => {
 					<p className='mb-20 text-gray-300'>
 						The JAMstack is a modern way of building websites. It takes away the
 						need for traditional databases and prebuilds your pages with the
-						most up-to-date data, so this can be served to visitors of your site
+						most up-to-date data, which are then served to visitors of your site
 						without making multiple costly server requests. This makes websites
-						faster, more secure and easy to scale. It is the perfect choice for
-						businesses of any size.
+						faster, more secure and easier to scale. It is the perfect choice
+						for any size of business.
 					</p>
 
 					<ul className='space-y-8 mb-20'>
@@ -58,7 +73,7 @@ const WebDevelopment = () => {
 							<h3 className='font-bold text-xl md:text-2xl mb-2'>Security</h3>
 							<p className='text-gray-300'>
 								No databases or third-party plugins for hackers to target means
-								your business and customer data will be remain safe and secure.
+								your business and customer data will remain safe and secure.
 							</p>
 						</li>
 						<li>
--- tailwind.config.js
+++ tailwind.config.js
@@ -43,5 +43,8 @@ module.exports = {
 			},
 		},
 	},
-	plugins: [require('@tailwindcss/typography')],
+	plugins: [
+		require('@tailwindcss/typography'),
+		require('@tailwindcss/aspect-ratio'),
+	],
 };
