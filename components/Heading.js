023918a3c44b75393a8379dfe801c59f2d42f4bf
Roy Sheppard <rsheppard83@gmail.com>
2022-05-03 22:55:27 +0100

    added ecommerce tools

--- components/Heading.js
+++ components/Heading.js
@@ -1,14 +1,24 @@
-const Heading = ({ title, dark }) => {
+const Heading = ({ heading, title, dark }) => {
 	return (
 		<>
 			<div className='h-2 w-1/2 bg-primary-100 mx-auto mb-10'></div>
-			<h1
-				className={`${
-					dark ? 'text-accent-100' : 'text-white'
-				} text-center text-3xl md:text-6xl font-futura font-bold mb-10`}
-			>
-				{title}
-			</h1>
+			{heading === 1 ? (
+				<h1
+					className={`${
+						dark ? 'text-accent-100' : 'text-white'
+					} text-center text-3xl md:text-6xl font-futura font-bold mb-10`}
+				>
+					{title}
+				</h1>
+			) : (
+				<h2
+					className={`${
+						dark ? 'text-accent-100' : 'text-white'
+					} text-center text-3xl md:text-6xl font-futura font-bold mb-10`}
+				>
+					{title}
+				</h2>
+			)}
 		</>
 	);
 };
--- pages/ecommerce-watford.js
+++ pages/ecommerce-watford.js
@@ -33,42 +33,32 @@ const Ecommerce = () => {
 					help you decide what are the best options for you and your target
 					market.
 				</p>
+
+				<div className='mb-20 text-white grid grid-cols-1 xs:grid-cols-2 md:grid-cols-4 justify-center items-center gap-10'>
+					<span className='flex justify-center'>
+						<FaShopify className='h-24 w-24 text-gray-300' />
+					</span>
+					<span className='flex justify-center'>
+						<FaCcPaypal className='h-24 w-24 text-gray-300' />
+					</span>
+					<span className='flex justify-center'>
+						<FaStripe className='h-24 w-24 text-gray-300' />
+					</span>
+					<span className='flex justify-center'>
+						<FaGooglePay className='h-24 w-24 text-gray-300' />
+					</span>
+				</div>
 			</section>
 
 			<Seperator dark={true} rotate={true} />
 			<section className='bg-accent-100 w-full'>
 				<div className='container py-20'>
-					<Heading title='Amazing e-commerce tools I work with' />
-
-					<div className='mt-20 text-white grid grid-cols-1 xs:grid-cols-2 md:grid-cols-4 justify-center items-center gap-10'>
-						<div className='mx-auto flex flex-col items-center gap-3'>
-							<FaShopify className='h-24 w-24 text-gray-300' />
-							<span className='w-fit bg-secondary-100/30 rounded py-2 px-3 text-sm'>
-								SHOPIFY
-							</span>
-						</div>
-
-						<div className='mx-auto flex flex-col items-center gap-3'>
-							<FaCcPaypal className='h-24 w-24 text-gray-300' />
-							<span className='w-fit bg-secondary-100/30 rounded py-2 px-3 text-sm'>
-								PAYPAL
-							</span>
-						</div>
-
-						<div className='mx-auto flex flex-col items-center gap-3'>
-							<FaStripe className='h-24 w-24 text-gray-300' />
-							<span className='w-fit bg-secondary-100/30 rounded py-2 px-3 text-sm'>
-								STRIPE
-							</span>
-						</div>
-
-						<div className='mx-auto flex flex-col items-center gap-3'>
-							<FaGooglePay className='h-24 w-24 text-gray-300' />
-							<span className='w-fit bg-secondary-100/30 rounded py-2 px-3 text-sm'>
-								GOOGLE PAY
-							</span>
-						</div>
-					</div>
+					<Heading heading={2} title='How e-commerce can help your business' />
+					<ul className='space-y-4 text-white mt-20'>
+						<li>Reason #1</li>
+						<li>Reason #2</li>
+						<li>Reason #3</li>
+					</ul>
 				</div>
 			</section>
 			<Seperator dark={true} />
