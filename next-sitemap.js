74df210ac413357f6342d331b31bc27ba9b211fb
Roy Sheppard <rsheppard83@gmail.com>
2022-05-23 14:49:56 +0100

    fix sitemap

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,5 +1,5 @@
 module.exports = {
-	siteUrl: process.env.SITE_URL || 'https://wwww.roysheppard.digital',
+	siteUrl: process.env.SITE_URL || 'https://www.roysheppard.digital',
 	generateRobotsTxt: true,
 	sitemapSize: 7000,
 	exclude: ['/slice-simulator'],
