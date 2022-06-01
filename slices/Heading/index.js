8dc84daef9bfd911b5cf73437bcc672bdc616e30
Roy Sheppard <rsheppard83@gmail.com>
2022-06-01 13:42:27 +0100

    fix css

--- slices/Heading/index.js
+++ slices/Heading/index.js
@@ -4,15 +4,20 @@ import { PrismicRichText } from '@prismicio/react';
 const Heading = ({ slice }) => {
 	return (
 		<div
-			className={`w-full scroll-mt-18 pt-${slice.primary.paddingTop} pb-${
-				slice.primary.paddingBottom
-			} ${
+			className={`w-full ${
 				slice.variation === 'headingDarkBackground'
 					? 'bg-accent-100'
 					: 'bg-white'
 			}`}
 		>
-			<div id={slice.primary.slug} className={`container`}>
+			<div
+				id={slice.primary.slug}
+				className={`container scroll-mt-18 ${
+					slice.variation === 'headingDarkBackground'
+						? 'pt-' + slice.primary.paddingTop
+						: 'mt-' + slice.primary.paddingTop || 'mt-20'
+				} pb-${slice.primary.paddingBottom || '20'}`}
+			>
 				<div className='h-2 w-1/2 bg-primary-100 mx-auto mb-10'></div>
 				<div
 					className={`${
