04c9119f10fadf3e2d6bd612ded882e0a5feaed3
Roy Sheppard <rsheppard83@gmail.com>
2022-06-05 10:40:12 +0100

    fix testimonial position

--- slices/Testimonial/index.js
+++ slices/Testimonial/index.js
@@ -8,10 +8,10 @@ const Testimonial = ({ slice }) => (
 	<section className='pb-20'>
 		<div className='container'>
 			<div className='flex w-full gap-6'>
-				<span className='hidden sm:flex justify-end items-end relative bottom-24 flex-grow w-full'>
+				<span className='hidden sm:flex justify-end items-end relative bottom-24 sm:flex-grow'>
 					<Quote className='fill-gray-400 h-20 w-20' />
 				</span>
-				<div className='flex flex-col items-center flex-initial sm:flew-grow'>
+				<div className='flex flex-col items-center flex-initial sm:w-full'>
 					<span className='relative'>
 						<Image
 							src={slice.primary.photo.url}
