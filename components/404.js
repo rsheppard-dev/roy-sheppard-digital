38660f12c646c96332a8595f0b8b7ea14de071d6
Roy Sheppard <rsheppard83@gmail.com>
2022-06-05 07:33:24 +0100

    fix 404

--- components/404.js
+++ components/404.js
@@ -1,8 +1,12 @@
+import Layout from './Layout';
+
 const PageNotFound = () => {
 	return (
-		<section className='container h-screen text-center my-auto'>
-			404: Sorry, that page does not exist.
-		</section>
+		<Layout>
+			<section className='container h-screen text-center flex justify-center items-center'>
+				404: Sorry, that page does not exist.
+			</section>
+		</Layout>
 	);
 };
 
