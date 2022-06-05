c5a0c849dae9ee53432d7bf00a571490fb15b7bc
Roy Sheppard <rsheppard83@gmail.com>
2022-06-05 10:32:55 +0100

    change bg

--- slices/Testimonial/index.js
+++ slices/Testimonial/index.js
@@ -5,11 +5,11 @@ import { PrismicRichText } from '@prismicio/react';
 import { Stars, Quote } from '../../components/Icons';
 
 const Testimonial = ({ slice }) => (
-	<section className='pb-20 bg-accent-100'>
+	<section className='pb-20'>
 		<div className='container'>
 			<div className='flex w-full gap-6'>
 				<span className='hidden sm:flex justify-end items-end relative bottom-24 flex-grow'>
-					<Quote className='fill-gray-600 h-20 w-20' />
+					<Quote className='fill-gray-400 h-20 w-20' />
 				</span>
 				<div className='flex flex-col items-center flex-initial sm:flew-grow'>
 					<span className='relative'>
@@ -21,7 +21,7 @@ const Testimonial = ({ slice }) => (
 							className='rounded-full grayscale'
 						/>
 					</span>
-					<span className='font-bold text-center text-white'>
+					<span className='font-bold text-center text-gray-900'>
 						{slice.primary.name}
 					</span>
 					<span className='font-bold text-center text-secondary-100 mb-10'>
@@ -29,11 +29,11 @@ const Testimonial = ({ slice }) => (
 					</span>
 				</div>
 				<span className='hidden sm:flex flex-grow justify-start items-end relative bottom-24'>
-					<Quote className='fill-gray-600 h-20 w-20 rotate-180' />
+					<Quote className='fill-gray-400 h-20 w-20 rotate-180' />
 				</span>
 			</div>
 			<div className='flex flex-col items-center justify-center'>
-				<span className='text-gray-300 text-center'>
+				<span className='text-gray-600 text-center'>
 					<PrismicRichText field={slice.primary.testimonial} />
 				</span>
 				<Stars className='text-primary-100 w-32' />
