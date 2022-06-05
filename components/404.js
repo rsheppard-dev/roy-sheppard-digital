c479a733e55faad220b60d4956f885e9887f5ba3
Roy Sheppard <rsheppard83@gmail.com>
2022-06-05 07:34:46 +0100

    fix 404

--- components/404.js
+++ components/404.js
@@ -1,12 +1,18 @@
+import Head from 'next/head';
 import Layout from './Layout';
 
 const PageNotFound = () => {
 	return (
-		<Layout>
-			<section className='container h-screen text-center flex justify-center items-center'>
-				404: Sorry, that page does not exist.
-			</section>
-		</Layout>
+		<>
+			<Head>
+				<title>404: Page Not Found</title>
+			</Head>
+			<Layout>
+				<section className='container h-screen text-center flex justify-center items-center'>
+					404 | Sorry, that page does not exist.
+				</section>
+			</Layout>
+		</>
 	);
 };
 
